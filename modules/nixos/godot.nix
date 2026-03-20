3c2562f5cf2f4ccc121bb50707b39db7fd314299
Матвей Мартыненко <darknekodev@yandex.ru>
2026-03-20 11:36:12 +0600

    add android to godot

--- modules/nixos/godot.nix
+++ modules/nixos/godot.nix
@@ -3,19 +3,52 @@
 	flake,
 	lib,
 	config,
+	inputs,
 	...
-}:
-flake.lib.mkMod {
-	inherit lib config;
-	name = "godot";
+}: let
+	user = config.mods.user.name or "user";
+	sdkPath = "/home/${user}/.android/sdk";
+in
+	flake.lib.mkMod {
+		inherit lib config;
+		name = "godot";
 
-	options = {};
+		options = {};
 
-	configs = {
-		environment.systemPackages = with pkgs; [
-			godot
-			godotPackages.export-template
-			gdscript-formatter
-		];
-	};
-}
+		home = {
+			imports = [inputs.android-nixpkgs.hmModule];
+			android-sdk.enable = true;
+
+			# Optional; default path is "~/.local/share/android".
+			android-sdk.path = sdkPath;
+
+			android-sdk.packages = sdkPkgs:
+				with sdkPkgs; [
+					build-tools-34-0-0
+					cmdline-tools-latest
+					platforms-android-34
+					platform-tools
+				];
+
+			home.sessionVariables = {
+				ANDROID_HOME = sdkPath;
+			};
+		};
+
+		configs = {
+			nixpkgs.overlays = [
+				inputs.android-nixpkgs.overlays.default
+			];
+
+			environment.systemPackages = with pkgs; [
+				godot
+				gdscript-formatter
+				openjdk21
+				android-tools
+			];
+
+			environment.variables = {
+				JAVA_HOME = "${pkgs.openjdk21}";
+			};
+		};
+	}
