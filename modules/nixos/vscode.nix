c827631dc6c12d704cb49d7320b884f3c10def9c
Матвей Мартыненко <darknekodev@yandex.ru>
2026-04-10 03:11:40 +0600

    rust+slint???

--- modules/nixos/vscode.nix
+++ modules/nixos/vscode.nix
@@ -31,16 +31,29 @@ flake.lib.mkMod {
 			profiles.default = {
 				enableUpdateCheck = false;
 				enableExtensionUpdateCheck = false;
-				extensions = with pkgs.vscode-extensions; [
-					jnoortheen.nix-ide
-					mkhl.direnv
-					christian-kohler.path-intellisense
-					dart-code.dart-code
-					dart-code.flutter
-					usernamehw.errorlens
-					foam.foam-vscode
-					geequlim.godot-tools
-				];
+				extensions = with pkgs.vscode-extensions;
+					[
+						jnoortheen.nix-ide
+						mkhl.direnv
+
+						christian-kohler.path-intellisense
+						usernamehw.errorlens
+						rust-lang.rust-analyzer
+					]
+					++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+						{
+							name = "slint";
+							publisher = "slint";
+							version = "1.15.1";
+							sha256 = "4kpW3bwXWrhWOslp/GQjCT2bv/kBCktsFo30orIII5U=";
+						}
+						# {
+						# 	name = "dms-theme";
+						# 	publisher = "danklinux";
+						# 	version = "0.0.3";
+						# 	sha256 = "MI1x1wiqvwg/N89oMuNVp0qlRT84ubvuMjtpkX0WKQY=";
+						# }
+					];
 
 				userSettings = {
 					"workbench.activityBar.location" = "top";
@@ -49,20 +62,21 @@ flake.lib.mkMod {
 					"workbench.layoutControl.enabled" = false;
 					"editor.lineNumbers" = "on";
 					"editor.glyphMargin" = false;
-					"workbench.editor.showTabs" = "single";
+					"workbench.editor.showTabs" = "multiple";
 					"window.menuBarVisibility" = "toggle";
 					"editor.cursorBlinking" = "solid";
 					"workbench.startupEditor" = "none";
 					"editor.formatOnSave" = true;
+					#"workbench.colorTheme" = "Dynamic Base16 DankShell (Dark)";
 
-					"json.schemaDownload.trustedDomains" = {
-						"https://schemastore.azurewebsites.net/" = true;
-						"https://raw.githubusercontent.com/" = true;
-						"https://www.schemastore.org/" = true;
-						"https://json.schemastore.org/" = true;
-						"https://json-schema.org/" = true;
-						"https://esm.sh/" = true;
-					};
+					# "json.schemaDownload.trustedDomains" = {
+					# 	"https://schemastore.azurewebsites.net/" = true;
+					# 	"https://raw.githubusercontent.com/" = true;
+					# 	"https://www.schemastore.org/" = true;
+					# 	"https://json.schemastore.org/" = true;
+					# 	"https://json-schema.org/" = true;
+					# 	"https://esm.sh/" = true;
+					# };
 
 					"direnv.restart.automatic" = true;
 
