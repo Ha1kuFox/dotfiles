8a6dce7197ebd8ff3f85dba21230c5030627ece7
Матвей Мартыненко <darknekodev@yandex.ru>
2026-04-26 03:19:55 +0600

    i3 startx

--- modules/nixos/i3.nix
+++ modules/nixos/i3.nix
@@ -1,17 +1,18 @@
 {
-	flake,
-	lib,
-	config,
-	...
+  flake,
+  lib,
+  config,
+  ...
 }:
 flake.lib.mkMod {
-	inherit lib config;
-	name = "i3";
+  inherit lib config;
+  name = "i3";
 
-	configs = {
-		services.xserver = {
-			enable = true;
-			windowManager.i3.enable = true;
-		};
-	};
+  configs = {
+    services.xserver = {
+      enable = true;
+      windowManager.i3.enable = true;
+      displayManager.startx.enable = true;
+    };
+  };
 }
